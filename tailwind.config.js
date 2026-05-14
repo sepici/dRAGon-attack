033de8c90ba75a7a87f6401e8c72d0323ce08825
Sep <sepici@gmail.com>
2026-05-14 17:29:23 +0300

    Tailwind: scan app/**/*.php so enum chipClasses() are compiled in

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        // Pick up dynamic class names returned from PHP (e.g. enum chipClasses()
+        // in app/Enums/Status.php and app/Enums/Moscow.php).
+        './app/**/*.php',
     ],
 
     theme: {
